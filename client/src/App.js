92289a43cf781708584c232834af651008cd914a
Didi518 <94155950+Didi518@users.noreply.github.com>
2023-04-16 18:53:30 +0200

    :sparkles: login/signup pages - front

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { LoginPage, SignupPage } from './Routes';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/connexion" element={<LoginPage />} />
+        <Route path="/inscription" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
