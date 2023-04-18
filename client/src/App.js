06bc25808d1b570d60520406b25f38b6f19d1d43
Didi518 <94155950+Didi518@users.noreply.github.com>
2023-04-18 03:04:59 +0200

    :lipstick: header frontend search & filter functionnalities

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { ActivationPage, LoginPage, SignupPage } from './Routes';
+import { ActivationPage, HomePage, LoginPage, SignupPage } from './Routes';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Store from './app/store';
@@ -15,6 +15,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/connexion" element={<LoginPage />} />
         <Route path="/inscription" element={<SignupPage />} />
         <Route
--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,5 +1,6 @@
 import LoginPage from './pages/Login';
 import SignupPage from './pages/Signup';
 import ActivationPage from './pages/ActivationPage';
+import HomePage from './pages/HomePage';
 
-export { ActivationPage, LoginPage, SignupPage };
+export { ActivationPage, HomePage, LoginPage, SignupPage };
